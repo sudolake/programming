235af3d19d74667d043469b5abdaecba4d777da1
Matej Krajcovic <matej.krajcovic3@gmail.com>
2022-06-23 13:11:49 +0200

    added gitignore

--- 100DaysPython/discord_bot/main.py
+++ 100DaysPython/discord_bot/main.py
@@ -37,6 +37,6 @@
 
 
 
-# token = OTg5NDgyMDY4NDYxMzg3Nzk2.GmlhZf.ItSd5-zdsKeAO2_WLgrtWj_yBWWrp0mGaA-pDU
+
 
 # add bot to server = https://discord.com/api/oauth2/authorize?client_id=989482068461387796&permissions=8&scope=bot
